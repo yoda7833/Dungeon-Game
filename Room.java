cccc5f32d31c249c7a70ed836896e1288ae271d2
killer8764 <rom99an@gmail.com>
2016-06-05 02:51:32 +0000

    Add files via upload

--- Room.java
+++ Room.java
@@ -7,95 +7,134 @@
  */
 public class Room
 {
-     // instance variables - replace the example below with your own
+    // instance variables - replace the example below with your own
     /*
      * 1 = wall, 0 = empty space, 2 = door , 3 = monster
      */
-    private char enter;
     private int[][] map;
     private int roomType;
+    private char enterPoint;
+    private int locationRow;
+    private int locationCol;
     /**
      *  Default constructor for objects of class RoomCorridor
      */
     public Room()
     {
-        roomType = (int)(Math.random()*3);
+        roomType = (int)(Math.random()*4);
+        enterPoint = 'g';
         switch(roomType){
             case 0:
-                    map = new int[5][(int)(Math.random()*10)+10];
-                    fillMap();
-                    break;
-            case 1:
-                    map = new int[5][5];
-                    fillMap();
-                    break;
-            case 2:
-                    map = new int[(int)(Math.random()*3)+10][(int)(Math.random()*3)+10];
-                    fillMap();
-                    break;
-            default:
-                    map = new int[3][3];
-                    fillMap();
-                    break;
-        }
-        //map = new int[5][(int)(Math.random()*10)+10];
-        //fillMap();
-    }
-    public Room(char type)
-    {
-        enter = type;
-        int random = (int)(Math.random()*100+1);
-        if(enter == 't'||enter == 'b')
-        {
-            map = new int[(int)(Math.random()*10)+10][5];
+            map = new int[5][(int)(Math.random()*10)+10]; //horizontal corridor
             fillMap();
+            break;
+            case 1:
+            map = new int[5][5]; //Small Room
+            fillMap();
+            break;
+            case 2:
+            map = new int[(int)(Math.random()*3)+10][(int)(Math.random()*3)+10]; //big Room
+            fillMap();
+            break;
+            case 3:
+            map = new int[(int)(Math.random()*10)+10][5]; //verticle corridor
+            fillMap();
+            break;
+            default:
+            map = new int[3][3];
+            fillMap();
+            break;
         }
-        else
-        {
-            
-        }
-        
-    }
-    /**
-     *  Constructor for objects of class RoomCorridor when given a RoomCorridor object
-     */
-    public Room(Room input)
-    {
-        for(int row =0; row<map.length;row++)
-            for(int col =0; col<map[0].length;col++)
-            {
-                map[row][col] = input.getMap()[row][col];
-            }
-        roomType = input.getRoomType();
     }
 
+    //used for knowing where to enter and place player
+    public Room(char enter)
+    {
+        enterPoint = enter;
+        int rand = (int)(Math.random()*3);
+        if(enterPoint == 'r' || enterPoint == 'l')
+        {
+            if(rand==0)
+            {
+                roomType=10;
+                map = new int[5][5]; //Small Room
+            }
+            else if(rand==1)
+            {
+                roomType=10;
+                map = new int[(int)(Math.random()*3)+10][(int)(Math.random()*3)+10]; //big Room
+            }
+            else
+            {
+                roomType=0;
+                map = new int[5][(int)(Math.random()*10)+10]; //horizontal corridor
+            }
+
+        }
+        else if(enterPoint == 't' || enterPoint == 'b')
+        {
+            if(rand==0)
+            {
+                roomType=10;
+                map = new int[5][5]; //Small Room
+            }
+            else if(rand==1)
+            {
+                roomType=10;
+                map = new int[(int)(Math.random()*3)+10][(int)(Math.random()*3)+10]; //big Room
+            }
+            else
+            {
+                roomType=3;
+                map = new int[(int)(Math.random()*10)+10][5]; //verticle corridor
+            }
+
+        }
+        //map = new int[5][5]; //Small Room
+        //map = new int[(int)(Math.random()*3)+10][(int)(Math.random()*3)+10]; //big Room
+        //map = new int[(int)(Math.random()*10)+10][5]; //verticle corridor
+        //map = new int[3][3];
+        fillMap();
+
+    }
+
+    public int getLocationRow()
+    {
+        return locationRow;
+    }
+    
+    public int getLocationCol()
+    {
+        return locationCol;
+    }
+    
     public int getRoomType()
     {
         return roomType;
     }
-    
-    public int roomX()
+
+    public int roomRow()
     {
         return map.length;
     }
-    
-    public int roomY()
+
+    public int roomCol()
     {
         return map[0].length;
     }
-    
+
     public int[][] getMap()
     {
         return map;
     }
-    
+
     public int changeMap(int row, int col, int change)
     {
         int changed = map[row][col];
         map[row][col]=change;
         return changed;
     }
-    
+
     private void fillMap()
     {
         //still needs work
@@ -107,35 +146,116 @@ public class Room
                 else
                     map[row][col] = 0;
             }
-        map[map.length/2][0]=2;
+        placeDoors();
+        placePlayer();
         fillMonster();
     }
-    
+
+    private void placePlayer()
+    {
+        if(enterPoint == 'l')
+        {
+            //spawn right
+            locationRow = roomRow()/2;
+            locationCol = roomCol() - 2;
+        }
+        else if(enterPoint == 'r')
+        {
+            //spawn left
+            locationRow = roomRow()/2;
+            locationCol = 2;
+        }
+        else if(enterPoint == 't')
+        {
+            //spawn bottom
+            locationRow = roomRow()-2;
+            locationCol = roomCol()/2;
+        }
+        else if(enterPoint == 'b')
+        {
+            //spawn top
+            locationRow = 2;
+            locationCol = roomCol()/2;
+        }
+    }
+
+    private void placeDoors()
+    {
+        if(roomType==0)//horizontal
+        {
+            if(enterPoint == 'l')
+            {
+                map[roomRow()/2][0] = 2;//left 
+            }
+            else if(enterPoint == 'r')
+            {
+                map[roomRow()/2][roomCol()-1] = 2;//right
+            }
+            else
+            {
+                map[roomRow()/2][0] = 2;//left 
+                map[roomRow()/2][roomCol()-1] = 2;//right
+            }
+        }
+        else if(roomType==3)//verticle
+        {
+            if(enterPoint == 'b')
+            {
+                map[roomRow()-1][roomCol()/2] = 2;//bottom
+            }
+            else if(enterPoint == 't')
+            {
+                map[0][roomCol()/2] = 2;//top
+            }
+            else
+            {
+                map[roomRow()-1][roomCol()/2] = 2;//bottom
+                map[0][roomCol()/2] = 2;//top
+            }
+        }
+        else//square
+        {
+            if(enterPoint == 't')
+            {
+                map[roomRow()/2][0] = 2;//left
+                map[roomRow()/2][roomCol()-1] = 2;//right
+                map[0][roomRow()/2] = 2;//top
+            }
+            else if(enterPoint == 'b')
+            {
+                map[roomRow()-1][roomCol()/2] = 2;//bottom
+                map[roomRow()/2][0] = 2;//left
+                map[roomRow()/2][roomCol()-1] = 2;//right
+            }
+            else if(enterPoint == 'l')
+            {
+                map[roomRow()/2][0] = 2;//left
+                map[0][roomCol()/2] = 2;//top
+                map[roomRow()-1][roomCol()/2] = 2;//bottom
+            }
+            else if(enterPoint == 'r')
+            {
+                map[roomRow()/2][roomCol()-1] = 2;//right
+                map[0][roomCol()/2] = 2;//top
+                map[roomRow()-1][roomCol()/2] = 2;//bottom
+            }
+            else
+            {
+                map[roomRow()/2][0] = 2;//left
+                map[roomRow()/2][roomCol()-1] = 2;//right
+                map[0][roomRow()/2] = 2;//top
+                map[roomRow()-1][roomCol()/2] = 2;//bottom
+            }
+        }
+    }
+
     private void fillMonster()
     {
         for(int row =0; row<map.length;row++)
             for(int col =0; col<map[0].length;col++)
             {
-                if(map[row][col]==0&&(int)(Math.random()*3)==0&&col!=1&&col!=map[0].length-2&&row!=1&&row!=map.length-2)
+                if(map[row][col]==0&&(int)(Math.random()*2)==0)
                     map[row][col] = 3;
             }
-        boolean isMonster=false;
-        for(int[] x: map)
-            for(int y: x)
-            {
-                if(y==3)
-                    isMonster=true;
-            }
-        if(isMonster==false)
-        {
-            for(int row =0; row<map.length;row++)
-                for(int col =0; col<map[0].length;col++)
-                {
-                    if(map[row][col]==2&&map[row+1][col]==2)
-                    {
-                        
-                    }
-                }
-        }
     }
 }
--- Run.java
+++ Run.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 /**
  * runs the game
  * 
@@ -17,39 +14,47 @@ public class Run
     static private boolean hideMonsters;
     static public void run()
     {
-        hideMonsters = false;
+        hideMonsters = true;
         reader = new Scanner(System.in);
         room = new Room();
-        player = new Character(room.getMap().length/2,room.getMap()[0].length-2);
+        player = new Character(room.getMap().length/2,room.getMap()[0].length/2);
         while(player.getHealth()>0)
         {
             printing();
-            //if(room.getMap()[player.getPosRow()][player.getPosCol()]==3)//will start battle
-            //System.out.println("You walked onto a monter");
+            if(room.getMap()[player.getPosRow()][player.getPosCol()]==3)//will start battle
+                System.out.println("You walked onto a monter");
             if(move().compareTo("q")==0)//causes
                 break;
             printing();
             if(room.getMap()[player.getPosRow()][player.getPosCol()]==1)
                 player.setHealth(-1);
             if(room.getMap()[player.getPosRow()][player.getPosCol()]==3)
-            {
-                boolean won = player.fighting();
-                printing();
-                if(won)
-                {
-                    room.changeMap(player.getPosRow(),player.getPosCol(),0);
-                }
-            }
+                System.out.println("You walked onto a monter");
             if(room.getMap()[player.getPosRow()][player.getPosCol()]==2)
             {
-                room = new Room();
-                player.setPos(room.getMap().length/2,room.getMap()[0].length-2);
+                if(player.getPosRow()==0)
+                {
+                    room = new Room('t');
+                }
+                else if(player.getPosRow() == (room.roomRow() - 1))
+                {
+                    room = new Room('b');
+                }
+                else if(player.getPosCol()==0)
+                {
+                    room = new Room('l');
+                }
+                else if(player.getPosCol() == (room.roomCol() - 1))
+                {
+                    room = new Room('r');
+                }
+                player.setPos(room.getLocationRow() ,room.getLocationCol());
             }
         }
         System.out.println();
         System.out.println("You Died Game Over");
     }
-
+    
     static private void printing()
     {
         System.out.println('\f');
@@ -73,47 +78,29 @@ public class Run
             System.out.println();
         }
     }
-
+    
     static private String move()
     {
         System.out.println("Input a WASD imput to move or q to quit");
         String n;
-        String k;
         //do{
-        do{
-            k = reader.nextLine(); // Scans the next token of the input as an int.
-        }while(k.length()<=0);
-        n = k.substring(0,1);
-        //}while(!(n.compareTo("w")==0||n.compareTo("a")==0||n.compareTo("s")!=0||n.compareTo("d")==0||n.compareTo("q")==0));
+             n = reader.next(); // Scans the next token of the input as an int.
+           // n = i.substring(0,1);
+        //}while(!(n.compareTo("w")==0||n.compareTo("a")==0||n.compareTo("s")!=0||n.compareTo("d")==0));
         switch(n){
             case "w": 
-            player.movePosRow(-1);
-            break;
+                     player.movePosRow(-1);
+                     break;
             case "a":
-            player.movePosCol(-1);
-            break;
+                    player.movePosCol(-1);
+                    break;
             case "s":
-            player.movePosRow(1);
-            break;
+                    player.movePosRow(1);
+                    break;
             case "d":
-            player.movePosCol(1);
-            break;
+                    player.movePosCol(1);
+                    break;
         }
         return n;
-    }
-
-    public static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("Dungeon Game");
-        frame.setSize(0,0);
-        frame.setLocation(500,500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel testLabel = new JLabel("Test");
-        testLabel.setPreferredSize(new Dimension(175, 100));
-        frame.getContentPane().add(testLabel, BorderLayout.CENTER);
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
     }
 }
